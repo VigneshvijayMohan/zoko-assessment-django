e7303e410ef43863d9c46148e267c02f43d0aad8
Vignesh Mohan <vigneshvijay15@gmail.com>
2025-05-21 01:59:56 +0530

    Added post api

--- message_app/urls.py
+++ message_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import get_all_messages
+from .views import get_all_messages, MessagesView
 
 
 urlpatterns = [
-    path("messages/", get_all_messages, name="messages")
+    path("messages/", get_all_messages, name="all_messages"),
+    # path("messages/", post_messages, name="send_messages"),
+    path("something/", MessagesView.as_view(), name="new" ),
+    # path("messages/<str:message_id>/read", status_change_messages, name="status_change_messages" )
 ]
--- message_app/views.py
+++ message_app/views.py
@@ -2,10 +2,58 @@ from django.shortcuts import render
 from .models import Message
 from .serializers import MessageSerializers
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 # Create your views here.
 
+class MessagesView(APIView):
+    def get(self, request):
+        user1 = request.GET.get("user1")
+        user2 = request.GET.get("user2")
+        
+        if not user1 or not user2:
+            return Response({"error": "Missing user1 or user2 in query params"}, status=400)
 
+        messages = Message.objects.filter(sender_id = user1, receiver_id = user2).order_by("timestamp") 
+        serialized_data = MessageSerializers(messages, many = True)
+        return Response(serialized_data.data)
+    def post(self, request):
+        data = request.data
+
+        sender_id = data.get("sender_id")
+        receiver_id = data.get("receiver_id")
+        content = data.get("content")
+
+        if not sender_id or not receiver_id or not content:
+            return Response({"error": "sender_id, receiver_id, and content are required."}, status=400)
+
+        message = Message.objects.create(
+            sender_id=sender_id,
+            receiver_id=receiver_id,
+            content=content
+        )
+
+        serialized = MessageSerializers(message)
+        return Response(serialized.data, status=201)
+
+
+
+# @api_view(["PATCH"])
+# def status_change_messages(request, message_id):
+#     data = request.data
+    
+#     if not data:
+#         return Response({"error": "Missing Data"}, status=400)
+
+#     message = Message.objects.get(message_id = message_id)
+#     print(message)
+#     if data.get("status") == "read":
+#         message.read = True
+#         message.save()
+#         serialized = MessageSerializers(message)
+#         return Response(serialized.data, status=200)
+#     else:
+#         return Response({"error": "Invalid data"}, status=400)
 
 @api_view(["GET"])
 def get_all_messages(request):
